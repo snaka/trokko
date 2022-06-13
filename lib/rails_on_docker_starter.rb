bf6b53382c31098eddcd4f1e393810b2c5dea43b
S.Nakamatsu <19329+snaka@users.noreply.github.com>
2022-06-13 09:06:11 +0900

    create gem

---
+++ lib/rails_on_docker_starter.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rails_on_docker_starter/version"
+
+module RailsOnDockerStarter
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/rails_on_docker_starter_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe RailsOnDockerStarter do
+  it "has a version number" do
+    expect(RailsOnDockerStarter::VERSION).not_to be nil
+  end
+end
